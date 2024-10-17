3533d8cd2b815f071fc767389e55e6f8d45b617b
Jeluskinha <jeanlucasgomesdeoliveira@gmail.com>
2024-10-17 08:05:59 -0300

    feat: componente de cadastro de usuarios

--- pages/users/index.php
+++ pages/users/index.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+include_once("../../venv.php");
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -42,7 +47,92 @@
     </div>
   </nav>
 
-  <h1>pagina de usuarios</h1>
+  <?php
+  if (isset($_SESSION['msg'])) {
+    echo $_SESSION['msg'];
+    unset($_SESSION['msg']);
+  }
+  ?>
+
+
+  <div style="height:80vh; display: flex; flex-direction: row;justify-content: space-evenly;align-items: center;">
+
+    <form action="./register/index.php" method="post">
+      <h1 style="margin-bottom: 50px">Cadastro de usuário</h1>
+      <div class="mb-3">
+        <label for="nome">Nome</label>
+        <input name="description" type="text" class="form-control" id="nome" placeholder="nome do nome">
+      </div>
+      <div class="mb-3">
+        <label for="email">Email</label>
+        <input name="description" type="email" class="form-control" id="email" placeholder="email@exemplo.com">
+      </div>
+      <div class="mb-3">
+        <label for="password">Senha</label>
+        <input name="description" type="password" class="form-control" id="password">
+      </div>
+      <button name="botaoCadastar" class="btn btn-primary" type="submit">Cadastrar</button>
+      <hr class="my-4">
+      <!-- <small class="text-body-secondary">By clicking Sign up, you agree to the terms of use.</small> -->
+    </form>
+
+    <!-- 
+    <div>
+      <?php
+      $sql = "SELECT * FROM tb_products";
+      $stmt = $conn->prepare($sql);
+      $stmt->execute();
+
+      $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+      if ($result) {
+        echo "<table class='table table-hover table-striped table-bordered'>";
+        echo "<tr><th>#</th><th>Descrição</th><th>Unidade</th><th style='text-align: center'>Ações</th></tr>";
+        foreach ($result as $row) {
+          $id = $row['id'];
+          echo "<tr>";
+          echo "<td>" . $row['id'] . "</td>";
+          echo "<td>" . $row['description'] . "</td>";
+          echo "<td style='text-align: center'> " . $row['unit'] . "</td>";
+          echo "<td>
+              <button onClick=\"location.href='update/index.php?id=$id'\" class='btn btn-success'>Editar</button>
+            </td>";
+          echo "</tr>";
+        }
+        echo "</table>";
+      } else {
+        $_SESSION['msg'] = "Nenhum produto cadastrado";
+        exit;
+      }
+      ?>
+    </div> -->
+
+  </div>
+
+
+
+  <!-- <nav class="navbar navbar-expand-lg navbar-light bg-light" style="position: absolute;bottom: 0;">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="../products/index.php">Produtos</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+        aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="../products/index.php">Cadastrar </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../_home/index.php">Atualizar </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../clifor/index.php">Listar</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav> -->
 
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
