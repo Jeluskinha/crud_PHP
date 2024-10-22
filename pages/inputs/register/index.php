b1025063c58f0ca3dba328f5244be8690724fbc3
Jeluskinha <jeanlucasgomesdeoliveira@gmail.com>
2024-10-22 10:00:50 -0300

    feat: ajustando bug em inputs

--- pages/inputs/register/index.php
+++ pages/inputs/register/index.php
@@ -11,26 +11,56 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['botaoCadastar'])) {
   $id_product = $_POST['id_product'];
   $amount = $_POST['amount'];
 
-  if (empty($amount)) { // se quantidade for fazia ou 0 retorna o errro
+  // echo '<pre>';
+  // print_r(empty($amount));
+  // die;
+
+  if (empty($amount) or $amount == 0) { // se quantidade for fazia ou 0 retorna o errro
     $_SESSION['msg'] = 'Informe a quantidade';
     header("Location: ../index.php");
   }
 
   try {
-    $sql = "INSERT INTO tb_input (id_clifor, id_user, id_product, amount, date) VALUES (:id_clifor, :id_user, :id_product, :amount, :date)";
 
-    $stmt = $conn->prepare($sql);
-
+    // Selecionando input para pegar a quantidade já existente
+    $input = "SELECT * FROM tb_input WHERE id_clifor = :id_clifor AND id_user = :id_user AND id_product = :id_product";
+    $stmt = $conn->prepare($input);
     $stmt->bindParam(':id_clifor', $id_clifor);
     $stmt->bindParam(':id_user', $id_user);
     $stmt->bindParam(':id_product', $id_product);
-    $stmt->bindParam(':amount', $amount);
-    $stmt->bindParam(':date', date('Y-m-d'));
     $stmt->execute();
 
-    $_SESSION['msg'] = 'Produto Cadastrado';
-    header("Location: ../index.php");
-    exit;
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    if (isset($result[0]) == 1) {
+      // Somando com o valor da entrada
+      $somaValores = $result[0]['amount'] + $amount;
+
+      // Atualizando QUANTIDADE 
+      $sql = "UPDATE tb_input SET amount = :amount WHERE id = :idInput";
+      $stmt = $conn->prepare($sql);
+      $stmt->bindParam(':amount',  $somaValores);
+      $stmt->bindParam(':idInput', $result[0]['id']);
+      $stmt->execute();
+
+      $_SESSION['msg'] = 'Entrada atualizada';
+      header("Location: ../index.php");
+    } else {
+      $sql = "INSERT INTO tb_input (id_clifor, id_user, id_product, amount, date) VALUES (:id_clifor, :id_user, :id_product, :amount, :date)";
+
+      $stmt = $conn->prepare($sql);
+
+      $stmt->bindParam(':id_clifor', $id_clifor);
+      $stmt->bindParam(':id_user', $id_user);
+      $stmt->bindParam(':id_product', $id_product);
+      $stmt->bindParam(':amount', $amount);
+      $stmt->bindParam(':date', date('Y-m-d'));
+      $stmt->execute();
+
+      $_SESSION['msg'] = 'Entrada registrada';
+      header("Location: ../index.php");
+      exit;
+    }
   } catch (PDOException $e) {
     $_SESSION['msg'] = "Erro: " . $e->getMessage();
     header("Location: ../index.php");
